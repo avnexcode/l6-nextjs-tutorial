c9edd49a216b9da2b9d36bed5715e66c73d3e750
avnexcode <axnvee18@gmail.com>
2025-02-08 22:45:22 +0700

    feat: implement renderElements utility for dynamic rendering in TodoTableBody

--- src/features/todo/tables/TodoTableBody.tsx
+++ src/features/todo/tables/TodoTableBody.tsx
@@ -1,33 +1,33 @@
+"use client";
 import { TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { TodoTableMenu } from "./TodoTableMenu";
+import { renderElements } from "@/utils/render-elements";
 
 export const TodoTableBody = () => {
+  const todoList = [...new Array<undefined>(0)];
   return (
     <TableBody>
-      <TableRow>
-        <TableCell>1</TableCell>
-        <TableCell className="w-full">Hello World</TableCell>
-        <TableCell>True</TableCell>
-        <TableCell>
-          <TodoTableMenu todoId="1" />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>1</TableCell>
-        <TableCell className="w-full">Hello World</TableCell>
-        <TableCell>True</TableCell>
-        <TableCell>
-          <TodoTableMenu todoId="1" />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>1</TableCell>
-        <TableCell className="w-full">Hello World</TableCell>
-        <TableCell>True</TableCell>
-        <TableCell>
-          <TodoTableMenu todoId="1" />
-        </TableCell>
-      </TableRow>
+      {renderElements({
+        of: todoList,
+        keyExtractor: (_, index) => index,
+        fallback: (
+          <TableRow>
+            <TableCell colSpan={4} className="text-center">
+              No Todo
+            </TableCell>
+          </TableRow>
+        ),
+        render: (_, index) => (
+          <TableRow>
+            <TableCell>{index + 1}</TableCell>
+            <TableCell className="w-full">Hello World</TableCell>
+            <TableCell>True</TableCell>
+            <TableCell>
+              <TodoTableMenu todoId="1" />
+            </TableCell>
+          </TableRow>
+        ),
+      })}
     </TableBody>
   );
 };
